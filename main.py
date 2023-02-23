5b1269c5ab197ff473ea630b9a026e6cc5c09cc2
bilalasghar931 <bilalasghar931@gmail.com>
2023-02-23 07:48:42 +0500

    demo projects of python

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-calculation_to_seconds = 24
-name_of_unit = "hours"
-
-def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculation_to_seconds} {name_of_unit}"
-
-def validate_and_execute():
-    try:
-        user_input_number = int(num_of_days_element)
-        if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
-            print(calculated_value)
-        elif user_input_number == 0:
-            print("you entered a 0, please enter a valid positive number")
-        else:
-            print("your entered a negative number, please enter a valid postive number")
-    except ValueError:
-        print("your input is not a valid number. Don't ruin my program!")
+from helper import validate_and_execute, user_input_message
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Hey user, enter number of days as a comma separated list and I will convert it to hours!\n")
-    for num_of_days_element in set(user_input.split(", ")):
-        validate_and_execute()
+    user_input = input(user_input_message)
+    days_and_unit = user_input.split(":")
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    validate_and_execute(days_and_unit_dictionary)
---
+++ python_data_types.py
@@ -0,0 +1,18 @@
+# String data type
+message = "enter some value"
+# Integer data type
+days = 20
+# Float data type
+price = 9.99
+# Boolean data type with true
+valid_number = True
+# Boolean data type with false
+exit_input = False
+# List data type with integer values
+list_of_days = [20, 40, 100, 20]
+# List data type with string values
+list_of_months = ["January", "February", "May"]
+# Set data type
+set_of_days = {20, 30, 50}
+# Dictionary data type
+days_and_unit = {"days": 10, "unit": "hours"}
